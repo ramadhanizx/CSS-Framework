fbc3933f83560469cdbc2720dc33a83e792ced36
ramadhanizx <159858517+ramadhanizx@users.noreply.github.com>
2024-04-03 08:26:44 +0700

    first commit

---
+++ java.js
@@ -0,0 +1,7 @@
+// Import the necessary components from MDB UI Kit
+import { Carousel, initMDB } from "mdb-ui-kit";
+
+// Initialize MDB UI Kit, passing the Carousel component
+initMDB({ Carousel });
+
+// Now, you can link the Carousel with your HTML structure
